1a75b2646346fd063cd77914a93ab5561dd54594
tarekhanfy <65197356+tarekzoka@users.noreply.github.com>
2022-10-08 01:50:25 +0200

    Add files via upload

--- tarekfeed.sh
+++ tarekfeed.sh
@@ -7,12 +7,12 @@
 #!/bin/sh
 #
 #
-wget -O /etc/opkg/user-feed.conf https://github.com/tarekzoka/settingsz/raw/main/user-feed.conf
+wget -O /etc/opkg/user-feed.conf https://github.com/tarekzoka/settingsz/blob/main/user-feed.conf
 wait
 opkg update
 wait
 sleep 2;
 exit 0
 
-
+https://github.com/tarekzoka/settingsz/blob/main/user-feed.conf
 
