d203a16810f024ba7ccf43873045cf574935da35
tarekhanfy <65197356+tarekzoka@users.noreply.github.com>
2023-01-13 22:18:03 +0200

    Add files via upload

--- tarekfeed2.sh
+++ tarekfeed2.sh
@@ -1,1 +1,7 @@
-ZWNobwojIS9iaW4vYmFzaAojIyMjIyMjIyMjIyMjIyMjIyMjIyMjIyMjIyMjIyMjIyMjIyMjIyMjIyMjIyMjIyMjIyMjIyMjIyMjIyMjIyMjIyMjIyMjIyMjIyMjIyMjIyMjIyMjIwojIyBDb21tYW5kPXdnZXQgaHR0cHM6Ly9yYXcuZ2l0aHVidXNlcmNvbnRlbnQuY29tL3RhcmVrem9rYS9zZXR0aW5nc3ovbWFpbi90YXJla2ZlZWQyLnNoIC1PIC0gfCAvYmluL3NoCiMjCmVjaG8Kd2dldCAtTyAvZXRjL29wa2cvdXNlci1mZWVkLmNvbmYgaHR0cHM6Ly9yYXcuZ2l0aHVidXNlcmNvbnRlbnQuY29tL3RhcmVrem9rYS9zZXR0aW5nc3ovbWFpbi9mZWVkL3VzZXItZmVlZC5jb25mCndhaXQKb3BrZyB1cGRhdGUKcmVib290CnNsZWVwIDI7CmV4aXQgMA==
+echo
+#!/bin/bash
+######################################################################################
+## Command=wget https://raw.githubusercontent.com/tarekzoka/settingsz/main/tarekfeed2.sh -O - | /bin/sh
+##
+echo
+d2dldCAtTyAvZXRjL29wa2cvdXNlci1mZWVkLmNvbmYgaHR0cHM6Ly9yYXcuZ2l0aHVidXNlcmNvbnRlbnQuY29tL3RhcmVrem9rYS9zZXR0aW5nc3ovbWFpbi9mZWVkL3VzZXItZmVlZC5jb25mCndhaXQKb3BrZyB1cGRhdGUKcmVib290CnNsZWVwIDI7CmV4aXQgMA==
