43b4cc92c934082a2e01b25ef04ff68f6e696093
tarekhanfy <65197356+tarekzoka@users.noreply.github.com>
2023-02-28 11:34:45 +0200

    Delete tarekfeed2.sh

--- tarekfeed2.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/sh
-#########
-echo 
-######################################################################################
-## Q29tbWFuZD13Z2V0IGh0dHBzOi8vcmF3LmdpdGh1YnVzZXJjb250ZW50LmNvbS90YXJla3pva2Evc2V0dGluZ3N6L21haW4vdGFyZWtmZWVkMi5zaCAtTyAtIHwgL2Jpbi9zaAojIwplY2hvCndnZXQgLU8gL2V0Yy9vcGtnL3VzZXItZmVlZC5jb25mIGh0dHBzOi8vcmF3LmdpdGh1YnVzZXJjb250ZW50LmNvbS90YXJla3pva2Evc2V0dGluZ3N6L21haW4vZmVlZC91c2VyLWZlZWQuY29uZgp3YWl0Cm9wa2cgdXBkYXRlCnJlYm9vdApzbGVlcCAyOwpleGl0IDA=
